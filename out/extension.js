89617a8ff96f1ad0121706ad54ad48f2fd410c6c
nomaanhusain <nomaanhusain@gmail.com>
2025-04-29 09:37:08 +0200

    launch functionality added

--- out/extension.js
+++ out/extension.js
@@ -180,6 +180,47 @@ function activate(context) {
     let launchFleet = vscode.commands.registerCommand('fleet-coder.launchFleet', () => {
         // TODO: Source the build (sourceAfterBuild) thing, then do the ros launch, add setting to define the launch file name and stuff
         vscode.window.showInformationMessage('Will execute ROS launch file on fleet');
+        const config = vscode.workspace.getConfiguration('fleet-coder');
+        const sourceAfterBuild = config.get('sourceAfterBuild') || 'source install/setup.bash';
+        const username = config.get('username') || 'thymio';
+        const password = config.get('password') || 'thymio';
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+        // Get workspace folder
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            vscode.window.showErrorMessage("No workspace detected! Open a workspace first.");
+            return;
+        }
+        const workspaceRoot = workspaceFolders[0].uri.fsPath;
+        const workspaceName = path.basename(workspaceRoot);
+        const csvFile = config.get('IPs') || path.join(workspaceRoot, 'ips.csv');
+        if (!fs.existsSync(csvFile)) {
+            vscode.window.showErrorMessage('CSV file not found! Add path to csv with IPs of remote devices in Fleet Coder Settings (\'Ctrl + ,\' search: Fleet Coder)');
+            return;
+        }
+        const destPath = config.get('workspacePath') || `/home/${username}/`;
+        const workspacePath = `${destPath}${workspaceName}`;
+        console.log("******** wrk path = " + workspacePath);
+        const ips = fs.readFileSync(csvFile, 'utf-8').split('\n').map(line => line.trim()).filter(line => line);
+        if (ips.length === 0) {
+            vscode.window.showErrorMessage("No IPs found in CSV file!");
+            return;
+        }
+        const packageName = config.get('packageName') || 'my_package';
+        const launchFile = "ros2 launch " + packageName + " " + config.get('launchFileName') || 'launch.py';
+        for (const ip of ips) {
+            const sshCmd = `sshpass -p "${password}" ssh -o StrictHostKeyChecking=no ${username}@${ip} "cd ${workspacePath} ${sourceAfterBuild} && ${launchFile}"`;
+            console.log("ssh source build= " + sshCmd);
+            vscode.window.showInformationMessage(`Launching on ${ip}`);
+            (0, child_process_1.exec)(sshCmd, (error, stdout, stderr) => {
+                if (error) {
+                    vscode.window.showErrorMessage(`Launching failed on ${ip}: ${stderr}`);
+                    return;
+                }
+                vscode.window.showInformationMessage(`launched on ${ip}`);
+            });
+        }
     });
     let syncWorkspace = vscode.commands.registerCommand('fleet-coder.syncWorkspace', () => {
         const config = vscode.workspace.getConfiguration('fleet-coder');
